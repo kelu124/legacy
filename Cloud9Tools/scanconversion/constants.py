09ebf55dd1143d8c8f3663589455a1d82da1f4f7
benchoufi <benchoufi.mehdi@gmail.com>
2016-05-12 18:59:02 +0200

    scanconversion in python, firstep to benchmark, @todo: benchmark against any submission

---
+++ Cloud9Tools/scanconversion/constants.py
@@ -0,0 +1,9 @@
+__author__ = 'mehdibenchoufi'
+
+
+IMAGE_SIZE_x = 512
+IMAGE_SIZE_z = 512
+
+NUM_IMG_DATA = 128
+NUM_SAMPLES = 1672
+OPENCV_RELATIVE_ANGLE = 512
---
+++ Cloud9Tools/scanconversion/main.py
@@ -0,0 +1,11 @@
+from filereader import FileReader
+from scanconverter import ScanConverter
+
+__author__ = 'mehdibenchoufi'
+
+def run():
+    file_reader = FileReader("data_kydney")
+    scanconversion = ScanConverter(file_reader)
+    scanconversion.convert(file_reader)
+
+run()
